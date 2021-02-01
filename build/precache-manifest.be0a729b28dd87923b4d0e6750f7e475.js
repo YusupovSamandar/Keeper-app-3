01b3a0193dadc666de704951bd5a9de0d6ea5d14
Samandar <samandar1031github@gmail.com>
2021-02-01 10:46:50 +0500

    add animation, upldated, made it well designed

---
+++ build/precache-manifest.be0a729b28dd87923b4d0e6750f7e475.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edd10142e84dde51f5cfacb09a6a762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4a43d774de9ce3a84d9",
+    "url": "/static/js/2.a75fd6b3.chunk.js"
+  },
+  {
+    "revision": "2abf2a688fdf79128719",
+    "url": "/static/js/main.3cb5a34b.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
